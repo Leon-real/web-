93b81053cb308904512e71d62cf27e1a7605a9a4
김경민 <tutmr999@hanyang.ac.kr>
2022-11-11 11:36:14 +0900

    오류페이지 만들기(500) urls.py파일 수정

--- common/views.py
+++ common/views.py
@@ -17,9 +17,3 @@ def signup(request):
         form = UserForm()
     return render(request, 'common/signup.html', {'form': form})
 
-
-def page_not_found(request, exception):
-    return render(request, 'common/404.html', {})
-
-# def page_not_found_500(request):
-#     return render(request, 'common/404.html', {})
